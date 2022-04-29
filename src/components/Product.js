2c94f307957b926f1c12d4b87332fd1e787bc1e5
Freja <frej400d@stud.kea.dk>
2022-04-29 16:32:46 +0200

    add function

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,16 @@
 export default function Product(props) {
+  function add() {
+    props.setBasket((oldState) => [...oldState, props.product]);
+  }
   return (
     <article>
-      <h2>{props.productdisplayname}</h2>
+      <h2>{props.product.productdisplayname}</h2>
       <img
-        src={`https://kea-alt-del.dk/t7/images/webp/640/${props.id}.webp`}
+        src={`https://kea-alt-del.dk/t7/images/webp/640/${props.product.id}.webp`}
         alt="product img"
       />
-      <p>{props.price}</p>
+      <p>{props.product.price}</p>
+      <button onClick={add}>Add to basket</button>
     </article>
   );
 }
