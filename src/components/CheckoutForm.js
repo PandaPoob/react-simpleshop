178b2a8f99337dc6c526dfaacc4d3a8f385758b3
Freja <frej400d@stud.kea.dk>
2022-04-29 17:07:36 +0200

    form

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,8 +1,21 @@
+import { useRef } from "react";
+
 export default function CheckoutForm() {
+  const formEl = useRef(null);
+  function submitted(e) {
+    e.preventDefault();
+    console.log(formEl.current.elements.fullname.value);
+    console.log(formEl.current.elements.address.value);
+  }
+
   return (
     <div>
-      <form>
-        <input></input>
+      <form ref={formEl} onSubmit={submitted}>
+        <label htmlFor="fullname">Full name</label>
+        <input required type="text" name="fullname" id="fullname"></input>
+        <label htmlFor="address">Address</label>
+        <input required type="text" name="address" id="address"></input>
+        <button>Pay</button>
       </form>
     </div>
   );
