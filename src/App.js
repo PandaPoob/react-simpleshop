f4d502b34d9f5796653b3441e9c264f536b41c5d
Freja <frej400d@stud.kea.dk>
2022-04-29 15:18:40 +0200

    comments

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import styles from "./App.module.css";
 
 function App() {
   const [products, setProducts] = useState([]);
-  useEffect(() => {
-    async function getProducts() {
-      const res = await fetch("https://kea-alt-del.dk/t7/api/products");
-      const data = await res.json();
-      setProducts(data);
-    }
-    getProducts();
-  }, []);
+  //navn på array - det er det der er state, navn på funktion - det skal kalde state (rebuilde)
+  useEffect(
+    () => {
+      //use effect gør at den kun kalder en enkelt gang, ellers ville den loope, da man ville kalde funktionen getproducts data(array) ville ændre sig og derved kalde setproducts igen
+      async function getProducts() {
+        const res = await fetch("https://kea-alt-del.dk/t7/api/products");
+        const data = await res.json();
+        setProducts(data);
+      }
+      getProducts();
+    },
+    [
+      //tomt array hvor man kan putte variables ind som hvis ændrede sig ville køre funktionen igen
+    ]
+  );
 
   return (
     <div className={styles.App}>
